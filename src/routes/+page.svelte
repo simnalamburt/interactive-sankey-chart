0baa3385d8522f7ec8428f1783462a472099388c
Hyeon Kim <simnalamburt@gmail.com>
2023-02-04 21:27:48 +0900

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const expenses = {
+		"Gross profit": {
+			"Operating profit": {
+				"Net profit": 4.7,
+				"Tax": 1.5,
+				"Interest": 0.3,
+			},
+			"Operating expenses": {
+				"R&D": 9.8,
+				"Sales & marketing": 4.6,
+				"General & admin": 3.1,
+			},
+		},
+		"Cost of revenue": 8.3,
+	};
+
+	function sumExpenses(expenses) {
+		let sum = 0
+		for (const [_, value] of Object.entries(expenses)) {
+			switch (typeof value) {
+				case 'number':
+					sum += value;
+					break;
+				case 'object':
+					sum += sumExpenses(value);
+					break;
+				default:
+					throw new Error('Unexpected type');
+			}
+		}
+		return sum;
+	}
+</script>
+
+<svg id=canvas>
+	<rect width=30 height={sumExpenses(expenses) * 30} fill="red" />
+</svg>
+
+<style>
+	svg#canvas {
+		display: block;
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
